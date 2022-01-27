ba3fb48d07427d9e643833ff8d02d5d3769c8ea8
luis1ribeiro <luismarioribeiro01@gmail.com>
2022-01-27 22:13:19 +0000

    commit

--- cadeiras/pl/comp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ls -lah > nothtml/fich.txt
-cd nothtml/
-./of < fich.txt > output.txt
-./adhtml < template.html > ../pl.html
-cd ..
-
--- cadeiras/so/comp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ls -lah > nothtml/fich.txt
-cd nothtml/
-./of < fich.txt > output.txt
-./adhtml < template.html > ../so.html
-cd ..
