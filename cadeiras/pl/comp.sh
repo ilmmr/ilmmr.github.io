a28ae65c8814ff87ccbdc4ead9691ec8216b48d2
luis1ribeiro <luismarioribeiro01@gmail.com>
2022-01-27 22:10:31 +0000

    up

--- cadeiras/pl/comp.sh
+++ cadeiras/pl/comp.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-ls -lah > notforgit/fich.txt
-cd notforgit/
+ls -lah > nothtml/fich.txt
+cd nothtml/
 ./of < fich.txt > output.txt
 ./adhtml < template.html > ../pl.html
 cd ..
-git add .
-git commit -m "plAlterado"
 
--- cadeiras/so/comp.sh
+++ cadeiras/so/comp.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-ls -lah > notforgit/fich.txt
-cd notforgit/
+ls -lah > nothtml/fich.txt
+cd nothtml/
 ./of < fich.txt > output.txt
 ./adhtml < template.html > ../so.html
 cd ..
-git add .
-git commit -m "soAlterado"
